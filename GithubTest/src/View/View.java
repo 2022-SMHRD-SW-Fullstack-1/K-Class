09b7447e0bdb5f3e1f2f953cd1c4d3f9d10992ec
smhrd <smhrd@DESKTOP-18C2J03>
2022-09-05 11:22:28 +0900

    뷰 수정/ 누리

--- GithubTest/src/View/View.java
+++ GithubTest/src/View/View.java
@@ -1,5 +1,7 @@
 package View;
 
+import java.util.Scanner;
+
 import Controller.movieCon;
 
 public class View {
@@ -7,8 +9,11 @@ public class View {
 	public static void main(String[] args) {
 
 		movieCon mc = new movieCon();
-
+		
 		mc.play();
+		
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
