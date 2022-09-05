517ee32dfe6fd1ba7ff427c67d1ac8417cf4ccfe
smhrd <smhrd@DESKTOP-AUGNTJ6>
2022-09-05 09:16:31 +0900

    09/05 9:16 - 강윤찬

--- GithubTest/src/Controller/GameController.java
+++ GithubTest/src/Controller/GameController.java
@@ -1,30 +1,39 @@
 package Controller;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import Model.MemberDAO;
+import Model.MemberDTO;
 
 public class GameController {
 
-	Scanner scan = new Scanner(System.in);
+	MemberDAO dao = new MemberDAO();
 
-	public static void GameMenu() { // 게임 시작 메뉴
-		System.out.println("[1]회원가입 [2]로그인 [3]종료");
+	int cnt = 0;
+
+	public int conInsert(String id, String pw, String nick, int age) {
+		// view가 넘겨준 회원가입에 대한 정보를 DAO로 연결해 주는 메소드
+		cnt = dao.insert(id, pw, nick, age);
+
+		return cnt;
 	}
 
-	public static void RegisterMenu(String id, String pw) { // 회원 가입 메뉴
-		System.out.print("아이디를 입력하세요");
-		while (true) {
-			if (id == id && pw == pw) { // 로그인 및 패스워드 정보가 맞다면
+	public ArrayList<MemberDTO> conSelect() {
 
-			} else { // 틀리다면
+		ArrayList<MemberDTO> totalList = new ArrayList<>();
 
-			}
+		totalList = dao.select();
 
-		}
-
+		return totalList;
 	}
 
-	public static void LoginMenu() { // 로그인 메뉴
+	// 로그인을 진행하기 위한 Controller 요청
+	public String conLogin(String id, String pw) {
 
+		String nick = dao.login(id, pw);
+
+		return nick;
 	}
 
 }
--- GithubTest/src/View/Main.java
+++ GithubTest/src/View/Main.java
@@ -2,23 +2,31 @@ package View;
 
 import java.util.Scanner;
 
+import Controller.GameController;
 import Model.ASCII;
 
 public class Main {
 
+	private static final String String = null;
+
 	public static void main(String[] args) {
 
 		Scanner scan = new Scanner(System.in);
-		
+
 		ASCII.GameStart();
+		GameController gc = new GameController();
 
 		while (true) {
-			System.out.println("[1]회원가입 [2]로그인 [3]종료");
+
+			gc.GameMenu();
 			int menu = scan.nextInt();
 
 			if (menu == 1) { // 회원가입
+				gc.RegisterMenu();		
 
 			} else if (menu == 2) { // 로그인
+				gc.LoginMenu(id, pw);
+				int sel = scan.nextInt();
 
 			} else if (menu == 3) { // 종료
 				System.out.println("퀴즈 종료");
@@ -27,7 +35,7 @@ public class Main {
 				System.out.println("다시 입력하세요");
 			}
 		}
-		
+
 		ASCII.GameEnd();
 
 	}
